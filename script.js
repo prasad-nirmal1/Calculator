876b0562d5d7f1754f81a3b8efc0d2ff0baf7707
Prasad Nirmal <prasadnirmal72@gmail.com>
2025-07-03 22:04:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const display = document.getElementById("display");
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    let expression = display.value
+      .replace(/÷/g, "/")
+      .replace(/×/g, "*");
+    display.value = eval(expression);
+  } catch {
+    display.value = "Error";
+  }
+}
